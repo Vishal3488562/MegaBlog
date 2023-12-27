4c9a4b35f99dee41a1e7d6a7d2794b80f2fa6842
Vishal Gupta <vishal3488562@gmail.com>
2023-12-27 11:53:25 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+      <img className='w-20 h-15' src="https://1000logos.net/wp-content/uploads/2020/08/Blogger-Logo-2010.png" alt="" />
+    </div>
+  )
+}
+
+export default Logo
